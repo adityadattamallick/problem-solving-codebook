13e5e97579c8d9b62c5fedef9c5fcc36bd6b417b
adityadattamallick <adityadatta30@gmail.com>
2025-06-10 22:01:04 +0600

    Solving

---
+++ code360-codingninjas/arrays/easy/n_2_forest.cpp
@@ -0,0 +1,12 @@
+void nForest(int n) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << "\n";
+    }
+
+}
+
+// N/2 Forest
