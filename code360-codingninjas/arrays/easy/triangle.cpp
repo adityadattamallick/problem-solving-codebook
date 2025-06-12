d053dc3318f776d6a5d4082873221d04190842c2
adityadattamallick <adityadatta30@gmail.com>
2025-06-12 23:40:40 +0600

    Solving

---
+++ code360-codingninjas/arrays/easy/triangle.cpp
@@ -0,0 +1,10 @@
+void triangle(int n) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << i + 1 << " ";
+        }
+        std::cout << "\n";
+    }
+
+}
