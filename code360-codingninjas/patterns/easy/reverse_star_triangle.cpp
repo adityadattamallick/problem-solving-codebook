b96fe2893bfab1e4eefa1d0db28a5cc83c84cccd
adityadattamallick <adityadatta30@gmail.com>
2025-08-19 04:55:55 +0600

    Solving

---
+++ code360-codingninjas/patterns/easy/reverse_star_triangle.cpp
@@ -0,0 +1,17 @@
+void nStarTriangle(int n) {
+    for (int i = 0; i < n; i++) { // Outer loop for the rows
+        // Space <-> Star <-> Space
+        // 3 inner loops for these, sequentially
+        // Space = i (till i, j < i in the condition); Star = ((2 * n) - ((2 * i) + 1))), or, after simplifying, Star = ((2 * n) - (2 * i) - 1); i starts at 0, i = 0
+        for (int j = 0; j < i; j++) {
+            std::cout << " ";
+        }
+        for (int j = 0; j < ((2 * n) - (2 * i) - 1); j++) {
+            std::cout << "*";
+        }
+        for (int j = 0; j < i; j++) {
+            std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+}
