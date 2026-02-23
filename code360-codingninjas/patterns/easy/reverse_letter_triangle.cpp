560824d335c8fb6a463d7cae35b31fb495598655
adityadattamallick <adityadatta30@gmail.com>
2026-02-23 21:55:11 +0600

    Solved one loop and logic related letter pattern problem

---
+++ code360-codingninjas/patterns/easy/reverse_letter_triangle.cpp
@@ -0,0 +1,10 @@
+void nLetterTriangle(int n) {
+    for (int i = n; i > 0; i--) {
+        int print_value = 65; // ASCII value for 'A' is 65
+        for (int j = 0; j < i; j++) {
+            std::cout << static_cast<char>(print_value) << " ";
+            print_value++; // Increment the print value for the next print by 1 to print the next character in order
+        }
+        std::cout << std::endl;
+    }
+}
